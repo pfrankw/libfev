1ad057babdec1b71d6fc4bd909a762b6845c50ba
Francesco Pompò <pfrankw@users.noreply.github.com>
2017-05-16 23:18:23 +0200

    Random tests

--- programs/test_loop.c
+++ programs/test_loop.c
@@ -1,27 +1,47 @@
 #include <stdio.h>
+#include <signal.h>
 
 #include <fev/fev.h>
 #include <fev/time.h>
+
+fev_t fev_g;
+static void sig_handler(int sig)
+{
+    if (sig == SIGINT) {
+        fev_free(&fev_g);
+        exit(0);
+    }
+}
 
 void timer(void *arg)
 {
     printf("TIMER!\n");
 }
 
+void timer2(void *arg)
+{
+    printf("TIMER2!\n");
+    fev_free(&fev_g);
+    exit(0);
+}
+
+
 int main()
 {
 
-    fev_t fev;
+    if (signal(SIGINT, sig_handler) == SIG_ERR)
+        return 0;
 
-    fev_init(&fev);
+    fev_init(&fev_g);
 
-    fev_add_timer(&fev, 1000, 0, timer, 0);
+
+    fev_add_timer(&fev_g, 1000, 0, timer, 0);
+    fev_add_timer(&fev_g, 5000, 0, timer2, 0);
 
     while (1) {
-        fev_run(&fev);
-        fev_sleep_usec(50*1000);
+        fev_run(&fev_g);
     }
 
 
-    fev_free(&fev);
+    fev_free(&fev_g);
 }
